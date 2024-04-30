fba73b5cd8dc9280966090ab1fc58697e53dd571
syh <yhoonsong96@gmail.com>
2024-04-30 15:45:52 +0900

    [Fix] PT 페이지 - 활성화 된 탭과 컨텐츠가 일치하지 않는 현상 수정

--- src/app/pt/layout.tsx
+++ src/app/pt/layout.tsx
@@ -1,15 +1,17 @@
 'use client';
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
 
   const { push } = useRouter();
+  const pathName = usePathname();
+  
 
   return (
     <main className='px-4'>
-      <Tabs defaultValue="findpt">
+      <Tabs value={pathName === '/pt/findpt' ? "findpt" : 'mypt'}>
         <div className='fixed w-full h-[50px] bg-neutral-100 top-[100px] left-0 md:left-[240px] flex items-center'>
           <TabsList className='w-full justify-start items-center md:w-[500px]'>
             <TabsTrigger className='w-1/2 h-[50px]' onClick={() => { push('/pt/findpt') }} value='findpt' >트레이너</TabsTrigger>
